d543352268ed2d38a2aa0562071dc76f5df7756e
jake <jeremiah.valero@hotmail.com>
2019-10-03 10:05:12 +0800

    Added code comments

--- server.py
+++ server.py
@@ -1,18 +1,23 @@
 import socket
+
+#Initializes the socket and sets it to non-blocking in order to allow concurrent connections.
 port = 4003
 host = socket.gethostbyname(socket.gethostname())              
 s = socket.socket()
 s.setblocking(False)
 s.bind((host, port))
 s.listen(1)
-
 print ('listening at %s %s' %(host,port))
+
+#Processes the '/register <password>' command. Registers the nickname currently in use.
 def register(nickname,conn,password):
     if nickname not in registered:
         registered[nickname] = password
         conn.send('Nickname successfully registered \n')
     else:
         conn.send('Nickname already registered \n')
+#Processes the '/login <nickname> <password>' command. Logs in to a registered nickname. 
+#Automatically transfer connection from previous user if nickname is used by another connection.
 def login(name,nickname,password,conn):
     if registered.get(nickname,'***') == password:
         users[nickname] = users.pop(name)
@@ -23,7 +28,7 @@ def login(name,nickname,password,conn):
     else:
         conn.send('Invalid password \n')
 
-
+#Broadcasts the message sent by the user. Loops through all other users that is not the sender and sends the message to their connection.
 def sendToAll(name,message):
     for recepient, conn in users.items():
         if recepient != name:
@@ -37,14 +42,14 @@ def sendToAll(name,message):
                 conn.send("<LF> \n")
             except socket.error:
                 pass
+#Processes the different '/' commands made by the user.
 def command(name,command,conn):
     try:
         comm = command.split(' ')[0]
         comm = comm.strip()
-        print (comm.strip() == '/online')
+        name = name.strip()
         if comm == '/register':
             comm,password = command.split(' ')
-            name = name.strip()
             password = password.strip()
             register(name,conn,password)
         elif comm == '/nick':
@@ -61,7 +66,6 @@ def command(name,command,conn):
                 conn.send('Nickname has already been registered. If you know the password to this nickname, use command "login <nickname> <password>" \n')
         elif comm =='/login':
             comm,nickname,password = command.split(' ')
-            name = name.strip()
             nickname = nickname.strip()
             password = password.strip()
             login(name,nickname,password,conn)
@@ -76,11 +80,13 @@ def command(name,command,conn):
     except:
         conn.send('Invalid command format \n')
         
-                
+            
 users = {}
 registered = {}
 counter = 0
+#Main function loop    
 while True:
+    #Listen for connections
     try:
         while True:
             try:
@@ -94,6 +100,7 @@ while True:
             print ('Got connection from', name)
             sendToAll('Admin','%s has connected \n'%name)   
             conn.send("You have now connected as "+name+". Here is a list of online users:"+str(users.keys())+" \n")
+        #Loop through the connections and listen for incoming messages
         for name, conn in users.items():
             try:
                 message = conn.recv(1024)
